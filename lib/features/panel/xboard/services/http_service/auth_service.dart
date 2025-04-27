d82adeeb42d1718a9152637050c39f5b2115e0af
zx2390096 <110877792+zx2390096@users.noreply.github.com>
2025-04-27 16:04:33 +0800

    Update auth_service.dart

--- lib/features/panel/xboard/services/http_service/auth_service.dart
+++ lib/features/panel/xboard/services/http_service/auth_service.dart
@@ -8,7 +8,7 @@ class AuthService {
     return await _httpService.postRequest(
       "/api/v1/passport/auth/login",
       {"email": email, "password": password},
-      requiresHeaders: false,
+      requiresHeaders: true,
     );
   }
 
