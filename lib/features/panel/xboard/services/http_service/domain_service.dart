d1cb653f33d44d5408369eca4af417e982e66486
zx2390096 <110877792+zx2390096@users.noreply.github.com>
2025-04-27 11:51:07 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class DomainService {
   static const String ossDomain =
-      'https://storage.googleapis.com/oss-clarity/config.json';
+      'https://api.jsonsilo.com/public/acb82956-f1f7-4bba-8ba5-721cc5027de3';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
   static Future<String> fetchValidDomain() async {
